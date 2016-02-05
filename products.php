43d323fd97f59ed50a52ae0ac447631b29d934ae
reiko1391@gmail.com <reiko1391@gmail.com>
2016-02-05 13:34:51 -0800

    changed the view of the index page

--- products.php
+++ products.php
@@ -17,13 +17,23 @@ loadScripts();
             $img = base64_encode($row['image']);
             
             // <img src="data:image/jpeg;base64,' . base64_encode($row2['image']) . '" width="290" height="290">'
-            $html .= "<tr>
-                        <td><input data-sku-qty='$sku' type='number' value='1' min='1' max='10' step='1'/></td>
-                        <td data-sku-price='$sku'>$price</td>
-                        <td data-sku-img='$sku'><img src='data:image/jpeg;base64,$img'/></td>
+            $html .= "
+            
+                        <ul style='float:left; margin:10px 0;list-style-type:none;'>
+                            <li data-sku-img='$sku'><img src='data:image/jpeg;base64,$img',width='250px' height='250px' /></li>
+                        </ul>
+            
+                        <ul style='float:left; margin:10px 0;list-style-type:none;'>
+                            
+                            <span>$<span data-sku-price='$sku'>$price</span></span>
+                            <li><input data-sku-qty='$sku' type='number' value='1' min='1' max='10' step='1' />
+                            <input data-sku-add='$sku' type='button' value='Add'/></li>
+                       
+                        </ul>
                         
-                        <td><input data-sku-add='$sku' type='button' value='Add'/></td>
-                      </tr>";
+                    
+                      
+                      ";
         }
         echo $html;
         return;
